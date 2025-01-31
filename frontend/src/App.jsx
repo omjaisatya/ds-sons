05a90582e60c3051b499f86bd99c4af1dd0875f8
omjaisatya <satyaprakash451256@gmail.com>
2025-01-31 17:18:51 +0530

    modify store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import cartReducer from "./store/cartSlice";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
@@ -10,12 +8,7 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
+import store from "./store/store";
 
 function App() {
   return (
@@ -25,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/products/:productId" element={<ProductDetailPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
