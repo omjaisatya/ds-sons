dd4aefbb32700ff095dc3fad08e1e766e52cc9df
omjaisatya <satyaprakash451256@gmail.com>
2025-02-01 19:44:55 +0530

    modify store redux

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     cart: cartReducer,
     products: productReducer,
   },
