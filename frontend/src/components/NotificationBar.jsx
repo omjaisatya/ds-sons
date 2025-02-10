75fe419a263aac64ac469d9fd731ffad0ec6c154
omjaisatya <satyaprakash451256@gmail.com>
2025-02-10 18:13:10 +0530

    add components

---
+++ frontend/src/components/NotificationBar.jsx
@@ -0,0 +1,13 @@
+const NotificationBar = () => {
+  return (
+    <div className="notification-container">
+      <div className="notification-text">
+        🚧 This website is currently under development. Some features may not
+        work properly. Please ensure you are logged in for a better experience
+        and to access all features. Stay tuned for updates! 🚀
+      </div>
+    </div>
+  );
+};
+
+export default NotificationBar;
