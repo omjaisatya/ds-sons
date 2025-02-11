2fd0040ef6adcdb301a72561eed869ad05df9d67
omjaisatya <satyaprakash451256@gmail.com>
2025-02-11 18:50:22 +0530

    add App Folder

---
+++ native-app/App/services/auth/firebaseConfig.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_APIKEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const createAdminUser = async (email, password) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  await setDoc(doc(db, "admins", userCredential.user.uid), {
+    email: email,
+    role: "admin",
+    createdAt: new Date(),
+  });
+
+  return userCredential.user;
+};
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
