0381c2817b5e4fa4c1d794cae5e80aa64294024d
omjaisatya <satyaprakash451256@gmail.com>
2025-02-12 21:49:13 +0530

    complete storejs

--- native-app/App/store/store.js
+++ native-app/App/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import authReducer from "./authSlice";
+import productReducer from "./productSlice";
+import cartReducer from "./cartSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    auth: authReducer,
+    cart: cartReducer,
+    products: productReducer,
   },
 });
