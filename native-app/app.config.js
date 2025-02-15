e8fc2f9ef94a242b73db66b6fd7835e5a1901e0f
omjaisatya <satyaprakash451256@gmail.com>
2025-02-15 14:34:35 +0530

    add and test github actions

---
+++ native-app/app.config.js
@@ -0,0 +1,13 @@
+export default {
+  expo: {
+    extra: {
+      expoPublicApiKey: process.env.EXPO_PUBLIC_APIKEY,
+      expoPublicAuthDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+      expoPublicProjectId: process.env.EXPO_PUBLIC_PROJECTID,
+      expoPublicStorageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+      expoPublicMessagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+      expoPublicAppId: process.env.EXPO_PUBLIC_APPID,
+      expoPublicMeasurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
+    },
+  },
+};
