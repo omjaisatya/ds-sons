42bd1707d43de5487bd8bed4bdd46212e09f80ad
omjaisatya <satyaprakash451256@gmail.com>
2025-02-17 07:24:20 +0530

    enhanced code or seperate for better

--- native-app/App.js
+++ native-app/App.js
@@ -1,12 +1,11 @@
 import { Provider as PaperProvider } from "react-native-paper";
 import { Provider } from "react-redux";
 import { store } from "./App/store/store";
-import { RootNavigator } from "./App/navigation";
+import "react-native-gesture-handler";
+import AppContent from "./AppContent";
 import { useEffect } from "react";
 import { restoreUser } from "./App/store/userSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   useEffect(() => {
@@ -22,8 +21,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PaperProvider>
-        <RootNavigator />
-        <StatusBar style="auto" />
+        <AppContent />
       </PaperProvider>
     </Provider>
   );
