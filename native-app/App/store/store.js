4a13c204411b1508948a85e560b4f38ba1799aa4
omjaisatya <satyaprakash451256@gmail.com>
2025-02-21 04:04:16 +0530

    add setting store

--- native-app/App/store/store.js
+++ native-app/App/store/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import authReducer from "./authSlice";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 import appReducer from "./appSlice";
+import settingsReducer from "./settingsSlice";
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +13,10 @@ export const store = configureStore({
     cart: cartReducer,
     products: productReducer,
     app: appReducer,
+    settings: settingsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
