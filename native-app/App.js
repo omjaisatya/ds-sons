3ce5fa88a4614f94203b1dba93be19605edf31d7
omjaisatya <satyaprakash451256@gmail.com>
2025-02-21 04:04:46 +0530

    add fonts

--- native-app/App.js
+++ native-app/App.js
@@ -1,14 +1,28 @@
-import { Provider as PaperProvider } from "react-native-paper";
+// import { Provider as PaperProvider } from "react-native-paper";
 import { Provider } from "react-redux";
 import { store } from "./App/store/store";
 import "react-native-gesture-handler";
 import AppContent from "./AppContent";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { restoreUser } from "./App/store/userSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import ThemeProvider from "./App/theme/ThemeProvider";
+import * as Font from "expo-font";
+import LoadingScreen from "./App/components/LoadingScreen";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": require("./assets/fonts/Inter_Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
     const checkUser = async () => {
       const user = await AsyncStorage.getItem("user");
       if (user) {
@@ -18,11 +32,15 @@ export default function App() {
     checkUser();
   }, []);
 
+  if (!fontsLoaded) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Provider store={store}>
-      <PaperProvider>
+      <ThemeProvider>
         <AppContent />
-      </PaperProvider>
+      </ThemeProvider>
     </Provider>
   );
 }
